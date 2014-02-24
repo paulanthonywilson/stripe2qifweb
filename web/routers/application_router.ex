b29861cff1b312832b1baaffeb8fae4740a6ba4b
Paul Wilson <paulanthonywilson@gmail.com>
2014-02-24 21:06:35 +0000

    redirect to https if on http and Heroku

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -1,14 +1,22 @@
+defmodule EnsureSSL do
+  def service(conn, fun) do
+    conn = conn.fetch([:cookies, :params, :headers])
+    if conn.req_headers["x-forwarded-proto"] == "http" do
+      ApplicationRouter.no_ssl(conn, conn.req_headers["host"])
+    else
+      fun.(conn)
+    end
+  end
+end
+
 defmodule ApplicationRouter do
   use Dynamo.Router
+  filter EnsureSSL
 
-  prepare do
-    # Pick which parts of the request you want to fetch
-    # You can comment the line below if you don't need
-    # any of them or move them to a forwarded router
-    # conn.fetch :req_headers
-    conn.fetch([:cookies, :params, :headers])
-    # conn.fetch(:req_headers)
+  def no_ssl(conn, host) do
+    redirect conn, to: "https://#{host}"
   end
+
 
   # It is common to break your Dynamo into many
   # routers, forwarding the requests between them:
